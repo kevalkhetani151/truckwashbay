95e11d67b87aab7ab71ea4046c756a7cbaf9f389
kevalkhetani151 <kevalkhetani15@gmail.com>
2026-08-17 22:20:15 +0530

    add logo

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,19 +11,19 @@ export default function RootLayout({ children }) {
           content="Professional truck washing services. Fast, reliable, and affordable truck cleaning solutions."
         />
 
-        {/* Favicon */}
-        <link rel="icon" href="./icon2.png" />
-        <link rel="shortcut icon" href="./icon2.png" />
-        <link rel="apple-touch-icon" href="./icon2.png" />
+        {/* Favicon / Browser Tab Icon */}
+        <link rel="icon" href="/logo3.png" />
+        <link rel="shortcut icon" href="/logo3.png" />
+        <link rel="apple-touch-icon" href="/logo3.png" />
 
-        {/* Optional */}
         <meta name="theme-color" content="#000000" />
+
         <meta property="og:title" content="Brizzy Truck Wash" />
         <meta
           property="og:description"
           content="Professional truck washing services."
         />
-        <meta property="og:image" content="/icon2.png" />
+        <meta property="og:image" content="/logo3.png" />
       </head>
 
       <body className="antialiased bg-black text-white font-sans">
